361ff829263270de06d32099953a8ddc0c3df937
John <jbschi02@louisville.edu>
2017-04-18 14:46:08 -0400

    edited Join Playlist screen to connect back to Menu Screen

--- app/src/main/java/com/cecs550/spotifyapp/Activities/Activities/JoinActivity.java
+++ app/src/main/java/com/cecs550/spotifyapp/Activities/Activities/JoinActivity.java
@@ -1,16 +1,38 @@
 package com.cecs550.spotifyapp.Activities.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cecs550.spotifyapp.R;
 
 public class JoinActivity extends AppCompatActivity {
-
+    private String accessToken;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        Intent intent = getIntent();
+        accessToken = intent.getStringExtra("token");
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_create_playlist);
+        cancelButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                GoToMenu(accessToken);
+            }
+        });
+    }
+
+    private void GoToMenu(String token)
+    {
+        Intent intent = new Intent(this, MenuActivity.class);
+        intent.putExtra("token", token);
+        startActivity(intent);
     }
 }
